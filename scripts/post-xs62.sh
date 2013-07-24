d6fb1cdecd35392c552b031725962fb796624c46
Takashi Sugano <takashi.sugano@gmail.com>
2013-07-24 18:09:32 +0900

    New file

---
+++ scripts/post-xs62.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+TOP=$1
+#
+# NTP
+sed -i -e 's/true/false/' ${TOP}/etc/sysconfig/clock
+sed -i -e '/HWCLOCK/s/no/yes/' ${TOP}/etc/sysconfig/ntpd
+echo broadcastclient > ${TOP}/etc/ntp.conf
+echo 172.16.1.1 >> ${TOP}/etc/ntp/step-tickers
+#
+# Post install script
+wget -O ${TOP}/etc/rc.d/init.d/postmaster http://172.16.1.1/scripts/postmaster
+chmod 755 ${TOP}/etc/rc.d/init.d/postmaster
+cd ${TOP}/etc/rc.d/rc3.d; ln -s ../init.d/postmaster S99zzpostmaster
+wget -O ${TOP}/root/cloud-setup-bonding.sh http://172.16.1.1/scripts/cloud-setup-bonding.sh
+#
+#
+exit 0
